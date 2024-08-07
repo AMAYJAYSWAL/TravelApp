b7cb7f65ac0a1fb6c720e4e7de9fb0c9f3edf17d
amay jayswal <amayjayswal@gmail.com>
2024-08-07 19:21:45 +0530

    review trip

--- app/create-trip/select-budget.jsx
+++ app/create-trip/select-budget.jsx
@@ -38,7 +38,7 @@ export default function SelectBudget(){
             return;
         }
 
-        router.push('');
+        router.push('/create-trip/review-trip');
     }
     return (
       <View style={{
